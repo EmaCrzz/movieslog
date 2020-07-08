e2b5bfeb8616b99a6919f73df1e31c00b7651a49
EmaCrzz <ema_villanueva@hotmail.com>
2020-07-07 21:44:54 -0300

    First part lightbox

--- static/js/movies.js
+++ static/js/movies.js
@@ -1,8 +1,8 @@
 function useMovies({ htmlContainer = null, movies = [] }) {
-  console.log(movies);
   if (htmlContainer.hasChildNodes()) {
     htmlContainer.innerHTML = "";
   }
+
   movies.map(movie => {
     const htmlItem = newMovieItem(movie);
     htmlContainer.insertAdjacentHTML("beforeend", htmlItem);
@@ -19,6 +19,23 @@ function useMovies({ htmlContainer = null, movies = [] }) {
       <small>${Type} - ${Year}</small>
     </div>`;
   }
+
+  document.querySelectorAll(".masonry-item").forEach(element =>
+    element.addEventListener("click", () => {
+      const movieSelected = movies.find(
+        movie => movie.imdbID === element.getAttribute("id")
+      );
+      const $ligthBox = document.getElementById("lightbox");
+      // const htmlContent = newMovieItem(movieSelected);
+      // $ligthBox.insertAdjacentHTML("beforeend", htmlContent);
+      $ligthBox.classList.toggle("u-is-hidden");
+    })
+  );
+
+  document.getElementById("button-lightbox").addEventListener("click", () => {
+    const $ligthBox = document.getElementById("lightbox");
+    $ligthBox.classList.toggle("u-is-hidden");
+  });
 }
 
 export { useMovies };
